37fb3ebbaeb5c11a8e92e41cab793e84aab15476
Jesus Miguel Mora Colmenares <jesusmiguelmora.dev@gmail.com>
2025-08-28 15:35:47 -0400

    Date functions moved to utilsDate

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
+export const getDaysMount = (daysMountNumber : number) =>  Array.from({length :daysMountNumber}, (_, index) => index + 1)
+const currentYear = new Date().getFullYear()
+export const GetYears = () => Array.from({length : 100}, (_,index) => currentYear - index)
+
 export function getDaysInCurrentYearMonth(month : number) {
   const year = new Date().getFullYear();
   return new Date(year, month + 1, 0).getDate();
 }
+
--- src/views/autentication/RegisterView.tsx
+++ src/views/autentication/RegisterView.tsx
@@ -1,5 +1,5 @@
 import { set, useForm } from "react-hook-form"
-import { getDaysInCurrentYearMonth } from "../../utils/dateUtils"
+import { getDaysInCurrentYearMonth, getDaysMount, GetYears } from "../../utils/dateUtils"
 import { useEffect, useState } from "react"
 
 const arrayMonths = [
@@ -21,9 +21,6 @@ function RegisterView() {
 
   const {register, handleSubmit, reset, formState: {errors}} = useForm()
   const [daysMountNumber, setDaysMountNumber] = useState(31)
-  const daysMount= Array.from({length :daysMountNumber}, (_, index) => index + 1)
-  const currentYear = new Date().getFullYear()
-  const years = Array.from({length : 100}, (_,index) => currentYear - index)
 
   const monthSelected = (e : React.ChangeEvent<HTMLSelectElement>) => {
     setDaysMountNumber(getDaysInCurrentYearMonth(parseInt(e.target.value)))
@@ -37,7 +34,7 @@ function RegisterView() {
       </div>
 
       <select id="day">
-        {daysMount.map((dayNumber) => {
+        {getDaysMount(daysMountNumber).map((dayNumber) => {
           return <option key={dayNumber} value={dayNumber}>{dayNumber}</option>
         })}
       </select>
@@ -49,7 +46,7 @@ function RegisterView() {
       </select>
 
       <select id="year" name="year">
-        {years.map((year) => {
+        {GetYears().map((year) => {
           return <option key={year} value={year}>{year}</option>
         })}
       </select>
