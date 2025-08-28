f1aaf35e0d45f44308c2f3aa72fbaff36bdeebf7
Jesus Miguel Mora Colmenares <jesusmiguelmora.dev@gmail.com>
2025-08-28 14:59:25 -0400

    added select year in register

--- src/views/autentication/RegisterView.tsx
+++ src/views/autentication/RegisterView.tsx
@@ -21,12 +21,13 @@ function RegisterView() {
 
   const {register, handleSubmit, reset, formState: {errors}} = useForm()
   const [daysMountNumber, setDaysMountNumber] = useState(31)
+  const daysMount= Array.from({length :daysMountNumber}, (_, index) => index + 1)
+  const currentYear = new Date().getFullYear()
+  const years = Array.from({length : 100}, (_,index) => currentYear - index)
+
   const monthSelected = (e : React.ChangeEvent<HTMLSelectElement>) => {
     setDaysMountNumber(getDaysInCurrentYearMonth(parseInt(e.target.value)))
   } 
-
-  const daysMount= Array.from({length :daysMountNumber}, (_, index) => index + 1)
-
 
   return (
     <div className="w-full h-screen bg-[#F0F2F5]">
@@ -44,6 +45,12 @@ function RegisterView() {
       <select id="month" onChange={monthSelected} name="month">
         {arrayMonths.map((month, index) => {
           return <option  key={index} value={index}>{month}</option>
+        })}
+      </select>
+
+      <select id="year" name="year">
+        {years.map((year) => {
+          return <option key={year} value={year}>{year}</option>
         })}
       </select>
       
